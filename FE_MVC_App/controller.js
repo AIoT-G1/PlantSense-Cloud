05f44cfdd92512244baaef3862770590400327bb
jaumeferr <36743497+jaumeferr@users.noreply.github.com>
2023-04-10 23:54:06 +0800

    update controller

--- FE_MVC_App/controller.js
+++ FE_MVC_App/controller.js
@@ -1,18 +1,42 @@
 var data = ""
-var current_plant_node_id = ""
+var current_plant = null
+var current_plant_idx = 0
 
 function init() {
     
     // Load data from backend
     data = getHttpAll();
+    load_current_plant()
+
     if (data) {
         // Display 1st row
         display_weather_values();
 
         // Display 2nd box
-        display_plant_detail_picture_user()
+        display_plant_detail_picture_user();
+        display_plant_detail_status();
+        display_plant_detail_information();
 
 
+    }
+}
+
+function load_current_plant() {
+    current_plant = {
+        "node_id": data["plants_info"][0]['plant_node_id'],
+        "name": data["plants_info"][0]['name'],
+        "description": data["plants_info"][0]['description'],
+        "disease": data["plants_info"][0]['disease'],
+        "moisture": 0,
+        "light": 0,
+        "last_watered": "yesterday"
+    }
+
+    for (var plant in data['plant_sensor_data']) {
+        if (plant['plant_node_id'] == current_plant['node_id']) {
+            current_plant['moisture'] = plant['moisture'];
+            current_plant['light'] = plant['light'];
+        }
     }
 }
 
@@ -63,7 +87,7 @@ function display_weather_values() {
         minValue: 0,
         width: 7,
         text: water_l,
-        colors: ['#00cde8', '#ab0505'],
+        colors: ['#eee', '#ab0505'],
         duration: 400,
         wrpClass: 'circles-wrp',
         textClass: 'circles-text',
@@ -73,7 +97,7 @@ function display_weather_values() {
 }
 
 
-// Plant detail picture
+/*** Plant detail picture ***/
 function onPlantDetailPictureButtonClick() {
     if (document.getElementById("PlantDetailPictureButton").className == "btn btn-primary btn-round"){
         display_plant_detail_picture_real();
@@ -87,15 +111,100 @@ function display_plant_detail_picture_real() {
     document.getElementById("PlantDetailPictureButton").className = "btn btn-primary btn-border btn-round";
     p_b64 = ""
     for (var plant in data['plants_info']) {
-        if (plant['plant_node_id'] == current_plant_node_id) {
+        if (plant['plant_node_id'] == current_plant['node_id']) {
             p_b64 = plant['photo_url']
         }
     }
 
-    // Display 
+// Display 
 }
 
 function display_plant_detail_picture_user() {
     document.getElementById("PlantDetailPictureButton").className = "btn btn-primary btn-round";
     document.getElementById("PlantDetailPictureButton").src = "asset/img/plant_sense_icon";
+}
+
+function display_plant_detail_information() {
+    document.getElementById("curr_plant_name").innerHTML = 
+}
+
+function display_plant_detail_status() {
+    var disease = "no";
+    var moisture = null;
+    var light = null; 
+    for (var plant in data['plants_info']) {
+        if (plant['plant_node_id'] == current_plant['node_id']) {
+            //disease = plant['disease'];
+        }
+    }
+
+    for (var values in data['plant_sensor_data']) {
+        if (values['plant_node_id'] == current_plant['node_id']) {
+            moisture = values['moisture']
+            light = values['light']
+        }
+    }
+
+    // Health
+    if (disease == "no") {
+        Circles.create({
+            id: 'circles-4',
+            radius: 45,
+            value: 1,
+            maxValue: 1,
+            minValue: 0,
+            width: 45,
+            colors: ['#eee', '#ab0505'],
+            duration: 400,
+            wrpClass: 'circles-wrp',
+            styleWrapper: true,
+        });
+    } else {
+        Circles.create({
+            id: 'circles-4',
+            radius: 45,
+            value: 0,
+            maxValue: 1,
+            minValue: 0,
+            width: 45,
+            colors: ['#eee', '#ab0505'],
+            duration: 400,
+            wrpClass: 'circles-wrp',
+            styleWrapper: true,
+        });
+    }
+
+    // Moisture
+    Circles.create({
+        id: 'circles-5',
+        radius: 45,
+        value: moisture,
+        maxValue: 1024,
+        minValue: 0,
+        width: 11,
+        text: moisture,
+        colors: ['#eee', '#177dff'],
+        duration: 400,
+        wrpClass: 'circles-wrp',
+        textClass: 'circles-text',
+        styleWrapper: true,
+        styleText: true
+    });
+
+    // Light
+    Circles.create({
+        id: 'circles-6',
+        radius: 45,
+        value: light,
+        maxValue: 256,
+        minValue: 0,
+        width: 11,
+        text: light,
+        colors: ['#eee', '#ab0505'],
+        duration: 400,
+        wrpClass: 'circles-wrp',
+        textClass: 'circles-text',
+        styleWrapper: true,
+        styleText: true
+    });    
 }
