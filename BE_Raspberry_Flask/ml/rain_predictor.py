df5de86bb220071f1883bafe49c9cd1e90b317ce
xueqi <72065687+dengxueqi@users.noreply.github.com>
2023-04-11 16:30:30 +0800

    string to float

--- BE_Raspberry_Flask/ml/rain_predictor.py
+++ BE_Raspberry_Flask/ml/rain_predictor.py
@@ -1,21 +1,27 @@
 from joblib import dump, load
 
+
 class rain_predictor:
-     
+
     def predict(self, temperature, humidity):
-        
+
         try:
-            
+
+            temperature = float(temperature)
+            humidity = float(humidity)
+
             # predict new temperature and humidity observation
             clf = load('rain-classifier.joblib')
-            print("to predict --> temp: " + str(temperature) + " ; humidity: " + str(humidity))
+            print("to predict --> temp: " +
+                  temperature + " ; humidity: " + humidity)
             # temperature, humidity
             newX = [[temperature, humidity]]
             result = clf.predict(newX)
-            print('Predict Rain: temp={}; humidity={}; rain={}'.format(temperature, humidity, result[0]))
+            print('Predict Rain: temp={}; humidity={}; rain={}'.format(
+                temperature, humidity, result[0]))
 
             return str(result[0])
-            
+
         except Exception as error:
 
             return 'Unknown'
